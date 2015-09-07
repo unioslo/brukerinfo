d5e4819f83197ac637fec6874c69883e325fb1c3
Torkild Gundersen Kjevik <torkild.kjevik@gmail.com>
2015-09-07 21:12:38 +0200

    CRB-961: Adds init.php from master branch once again due to unattentive file adding to commits.

--- www_docs/init.php
+++ www_docs/init.php
@@ -37,7 +37,6 @@
  */
 
 // Get InitBase before Init, since autoload is not set up yet
-require_once '/www/var/virtual/w3utv-ws02.uio.no_443/htdocs/cerebrum/tgk/phplib/vendor/autoload.php';
 require_once dirname(__FILE__) . '/config.php';
 require_once LINK_LIB . '/controller/InitBase.php';
 
