89e47387ab37297ca7457ccccc8fd37f45d36bf2
Sivert Hatteberg <sivert.hatteberg@usit.uio.no>
2018-12-05 13:44:30 +0100

    Remove feature toggle

--- system/controller/Authorization/uio.php
+++ system/controller/Authorization/uio.php
@@ -133,15 +133,6 @@ class Authorization_uio extends Authorization
             return false;
         }
 
-        // Feature toggle. Limit the use to only INT + karinefu for now
-        $allowed_users = array('skh', 'ae', 'fhl', 'hanskfje',  'hmo',  'jbr', 'jokim', 'jsama', 'raymonm', 'sgs', 'karinefu');
-        $username = $this->user->getUsername();
-        if (in_array($username, $allowed_users)) {
-            return true;
-        } else {
-            return false;
-        }
-
         $affs = $this->bofh->getCache('affiliations');
         foreach($affs as $aff) {
 
