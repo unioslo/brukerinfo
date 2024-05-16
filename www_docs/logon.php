bdab7300644986d1268f44712441e0a5e94909fc
Eilif Bencze <eilifb@uio.no>
2024-05-16 10:22:49 +0200

    Remove passord.uio infobox with smaller forgotten passoword link

--- www_docs/logon.php
+++ www_docs/logon.php
@@ -1,18 +1,18 @@
 <?php
 // Copyright 2009, 2010, 2011 University of Oslo, Norway
-// 
+//
 // This file is part of Cerebrum.
-// 
+//
 // Cerebrum is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // Cerebrum is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with Cerebrum. If not, see <http://www.gnu.org/licenses/>.
 
@@ -84,7 +84,6 @@ $View->addTitle(txt('LOGON_TITLE'));
 $View->setFocus('#usi'); //TODO: move setfokus to Bofhform maybe?
 $View->start();
 
-$View->addElement('raw', txt('logon_intro'));
 $View->addElement($logform);
 $View->addElement('raw', '<div id="logon-outro">' . txt('logon_outro') . '</div>');
 
